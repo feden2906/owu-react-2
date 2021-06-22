08c8fb0244c46610019d76d3761fd7bb480e6c75
iFatumm <moshurenko@gmail.com>
2021-06-22 20:06:42 +0300

    hw 1

---
+++ src/components/posts/Posts.js
@@ -0,0 +1,23 @@
+import {useEffect, useState} from "react";
+
+export default function Posts() {
+
+    let [postsList, setPostsList] = useState([]);
+
+
+    useEffect(() => {
+       fetch('https://jsonplaceholder.typicode.com/posts')
+           .then(value => value.json())
+           .then(response => {
+               console.log(response)
+               setPostsList(response);
+           });
+    }, []);
+     return (
+       <div>
+            {
+                postsList.map(value => <div>{value.id} - {value.body}</div>)
+            }
+        </div>
+    );
+}
